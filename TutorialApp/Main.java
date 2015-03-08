9bbb84da6cde207c392955ab01a1c4496fb91b81
Xavier Galloway <xgalloway3@gatech.edu>
2015-03-08 00:21:47 -0500

    Update Main.java

--- TutorialApp/Main.java
+++ TutorialApp/Main.java
@@ -6,16 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 800));
+        primaryStage.setTitle("TOO TO DO");
         primaryStage.show();
-    }
 
+
+    }
 
     public static void main(String[] args) {
         launch(args);
